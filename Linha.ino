d80858874a8fd313a835832625641ba9ab2ee3f2
Anna Leticia Alegria <annaalegria23@hotmail.com>
2018-05-25 15:49:55 -0300

    Adicionando arquivo de report

---
+++ Linha.ino
@@ -0,0 +1,33 @@
+const byte left_sensor=11;
+const byte right_sensor=3;
+const byte front_sensor=A0;
+
+void setup ()
+{
+   pinMode(left_sensor,INPUT);
+   pinMode(right_sensor,INPUT);
+   pinMode(front_sensor,INPUT);
+   
+   Serial.begin(9600);  
+   Serial.println("starting");
+   delay(1000);
+}
+
+void loop()
+{
+  Line_check();
+  delay(100);
+}
+  
+  void Line_check ()
+{
+
+      if ( digitalRead (left_sensor) == LOW && digitalRead(right_sensor)==LOW && digitalRead(front_sensor)==LOW ) {Serial.print("Line_left"); Serial.print("\t"); Serial.print("Line_right"); Serial.print("\t"); Serial.println("Line_front");}
+      if ( digitalRead (left_sensor) == HIGH && digitalRead(right_sensor)==LOW && digitalRead(front_sensor)==LOW) {Serial.print("None"); Serial.print("\t"); Serial.print("Line_right");Serial.print("\t"); Serial.println("Line_front");}
+      if ( digitalRead (left_sensor) == LOW && digitalRead(right_sensor)==HIGH && digitalRead(front_sensor)==LOW ) {Serial.print("Line_left"); Serial.print("\t"); Serial.print("None");Serial.print("\t"); Serial.println("Line_front");}
+      if ( digitalRead (left_sensor) == LOW && digitalRead(right_sensor)==LOW && digitalRead(front_sensor)==HIGH ) {Serial.print("Line_left"); Serial.print("\t"); Serial.print("Line_right");Serial.print("\t"); Serial.println("None");}
+      if ( digitalRead (left_sensor) == HIGH && digitalRead(right_sensor)==HIGH && digitalRead(front_sensor)==LOW ) {Serial.print("None"); Serial.print("\t"); Serial.print("None");Serial.print("\t"); Serial.println("Line_front");}
+      if ( digitalRead (left_sensor) == HIGH && digitalRead(right_sensor)==LOW && digitalRead(front_sensor)==HIGH ) {Serial.print("None"); Serial.print("\t"); Serial.print("Line_right");Serial.print("\t"); Serial.println("none");}
+      if ( digitalRead (left_sensor) == LOW && digitalRead(right_sensor)==HIGH && digitalRead(front_sensor)==HIGH ) {Serial.print("Line_left"); Serial.print("\t"); Serial.print("none");Serial.print("\t"); Serial.println("none");}
+      if ( digitalRead (left_sensor) == HIGH && digitalRead(right_sensor)==HIGH && digitalRead(front_sensor)==HIGH ) {Serial.print("none"); Serial.print("\t"); Serial.print("none");Serial.print("\t"); Serial.println("none");}
+}
---
+++ report.md
@@ -0,0 +1,6 @@
+Semana 28/05/2018 - 03/06/2018
+
+    - Planejamentos:
+        - Conseguir os sensores para testar os códigos
+        - Reescrever códigos de teste do MiniMais
+        - Reescrever códigos de teste do MiniPuma+
